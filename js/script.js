17ca0007594c740807df3a5bcb5bb0741fed458f
yhist <yhyh0903@gmail.com>
2022-06-23 23:30:05 +0900

    스와이퍼 버튼, infom 수정, 카트 아이콘 호버

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ $(document).ready();
 window.onload = function () {
   new Swiper('.sw-infom', {
     loop: true, 
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 1000,
+    //   disableOnInteraction: false,
+    // },
     speed: 1000,
     navigation: {
-      prevEl: '.sw-visual-prev',
-      nextEl: '.sw-visual-next'
+      prevEl: '.sw-infom-prev',
+      nextEl: '.sw-infom-next'
+    },
+
+    pagination: {
+      el: '.sw-infom-pg',
+      type: 'fraction'
     }
   })
 }
