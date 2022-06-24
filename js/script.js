4fdbc139c826a60f16db0b8328a2a69f2a00a85c
yhist <yhyh0903@gmail.com>
2022-06-24 15:21:06 +0900

    정리

--- js/script.js
+++ js/script.js
@@ -2,11 +2,7 @@ $(document).ready();
 
 window.onload = function () {
   new Swiper('.sw-infom', {
-    loop: true, 
-    // autoplay: {
-    //   delay: 1000,
-    //   disableOnInteraction: false,
-    // },
+
     speed: 1000,
     navigation: {
       prevEl: '.sw-infom-prev',
